9bf1abc16381c5f914d5b0eb9032fee685e5503f
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-18 18:33:05 +0530

    ADDED: calculated fare

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+
+	private static final double MIN_COSY_PER_KM = 10;
+	private static final int MIN_COST_PER_MINUTE = 1;
+	private static final double MIN_CAB_FARE = 5;
+	
+
+	public double totalFare(double distance, int time) {
+		double fare = distance *MIN_COSY_PER_KM + time * MIN_COST_PER_MINUTE;
+		double totalCabFare = Math.max(MIN_CAB_FARE, fare);
+		return totalCabFare;
+		
+	}
+
+}
