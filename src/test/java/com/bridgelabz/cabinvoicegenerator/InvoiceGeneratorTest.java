33c998e929ac095880595c08fb20ed03ef0ddc06
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-18 18:41:44 +0530

    UPDATED:test case for enhanced invoice fare

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -10,7 +10,6 @@ public class InvoiceGeneratorTest {
 	@Before
 	public void setUp() {
 		generator = new InvoiceGenerator();
-
 	}
 	@Test
 	public void whenDistanceAndTimeGiven_ShouldReturnTotalFare() {
@@ -27,11 +26,21 @@ public class InvoiceGeneratorTest {
 		assertEquals(5,fare,0.0);
 	}
 	@Test
-	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+	public void givenMultipleRides_ShouldReturnInvoice() {
 
 		Ride[] rides = {new Ride(2.0, 5), 
 						new Ride(0.1, 1)};
 		double totalFare = generator.calculateFare(rides);
 		assertEquals(30, totalFare, 0.0);
+	}
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+
+		Ride[] rides = {new Ride(2.0, 5), 
+						new Ride(0.1, 1)};
+		InvoiceSummary summary = generator.calculateFareReturnObject(rides);
+		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+		if(expectedSummary.getAverageFare() == summary.getAverageFare() && expectedSummary.getNumberOfRides() == summary.getNumberOfRides() && expectedSummary.getTotalFare() == summary.getTotalFare())
+			assertEquals(1, 1);
 	}
 }
